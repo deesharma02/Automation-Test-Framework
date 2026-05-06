3fba898759d70329999dfab6ae9a3a33e8c8f66e
Deepak Sharma <dabaangdeepak2@gmail.com>
2026-05-06 19:11:39 +0530

    Made changes in testng xml file

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -13,21 +13,21 @@ import utilities.LoggerUtilities;
 @Listeners({listeners.TestListeners.class})
 public class LoginTest extends BaseTest {
 
-//    @Test(description = "Verify Login with valid credential" , dataProviderClass =
-//            dataProviders.LoginDataProvider.class , dataProvider = "LoginTestDataProvider" , retryAnalyzer = listeners.MyRetryAnalyzer.class)
-//    public void VerifyLoginTestJSON(User user) {
-//        String userName =
-//                homePage.ClickOnSignIn().doLoginWithCredentials(user.getEmail() , user.getPassword()).visibleText();
-//        Assert.assertEquals(userName , "John Doe");
-//    }
+    @Test(description = "Verify Login with valid credential" , dataProviderClass =
+            dataProviders.LoginDataProvider.class , dataProvider = "LoginTestDataProvider" , retryAnalyzer = listeners.MyRetryAnalyzer.class)
+    public void VerifyLoginTestJSON(User user) {
+        String userName =
+                homePage.ClickOnSignIn().doLoginWithCredentials(user.getEmail() , user.getPassword()).visibleText();
+        Assert.assertEquals(userName , "John Doe");
+    }
 
-//    @Test(description = "Verify Login with valid credential" , dataProviderClass =
-//            dataProviders.LoginDataProvider.class , dataProvider = "LoginCsvTestDataProvider" , retryAnalyzer = listeners.MyRetryAnalyzer.class)
-//    public void VerifyLoginTestCSV(User user) {
-//        String userName =
-//                homePage.ClickOnSignIn().doLoginWithCredentials(user.getEmail() , user.getPassword()).visibleText();
-//        Assert.assertEquals(userName , "John Doe");
-//    }
+    @Test(description = "Verify Login with valid credential" , dataProviderClass =
+            dataProviders.LoginDataProvider.class , dataProvider = "LoginCsvTestDataProvider" , retryAnalyzer = listeners.MyRetryAnalyzer.class)
+    public void VerifyLoginTestCSV(User user) {
+        String userName =
+                homePage.ClickOnSignIn().doLoginWithCredentials(user.getEmail() , user.getPassword()).visibleText();
+        Assert.assertEquals(userName , "John Doe");
+    }
 
     @Test(description = "Verify Login with Invalid credential")
     public void VerifyLoginTestWithInvalidCredentialsV() {
